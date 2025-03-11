4f0a08df7fd0cc6d54d56a0466a8eca1493a3c08
Admin2432 <iv1231211@gmail.com>
2025-03-12 00:05:13 +0300

    Update updates.js

--- update-server/api/updates.js
+++ update-server/api/updates.js
@@ -2,7 +2,9 @@ export default function handler(req, res) {
     res.status(200).json({
         "games": {
             "game1": { "version": "1.2", "url": "https://download-link.com/game1/latest.apk" },
-            "game2": { "version": "2.0", "url": "https://download-link.com/game2/latest.apk" }
+            "game2": { "version": "2.0", "url": "https://download-link.com/game2/latest.apk" },
+            "game3": { "version": "1.0", "url": "https://download-link.com/game3/latest.apk" }, // Добавленная игра
+            "game4": { "version": "1.5", "url": "https://download-link.com/game4/latest.apk" }  // Добавленная игра
         }
     });
 }
